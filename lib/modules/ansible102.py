d7acf5e6110d7c12cd42c5e53d891df769a51b35
Nathan Isburgh <nathan.isburgh@edgecloud.com>
2021-07-15 18:27:44 +0000

    Updated custom module to implement lab requirements ( triggers task dump )

--- lib/modules/ansible102.py
+++ lib/modules/ansible102.py
@@ -71,8 +71,7 @@ from ansible.module_utils.basic import AnsibleModule
 def run_module():
     # define available arguments/parameters a user can pass to the module
     module_args = dict(
-        name=dict(type='str', required=True),
-        new=dict(type='bool', required=False, default=False)
+        trigger_path=dict(type='str', required=False, default='/proc/sysrq-trigger')
     )
 
     # seed the result dict in the object
@@ -82,7 +81,6 @@ def run_module():
     # for consumption, for example, in a subsequent task
     result = dict(
         changed=False,
-        original_message='',
         message=''
     )
 
@@ -99,23 +97,13 @@ def run_module():
     # want to make any changes to the environment, just return the current
     # state with no modifications
     if module.check_mode:
+        result['message'] = 'Dump NOT triggered because of check mode'
         module.exit_json(**result)
 
-    # manipulate or modify the state as needed (this is going to be the
-    # part where your module will do what it needs to do)
-    result['original_message'] = module.params['name']
-    result['message'] = 'goodbye'
-
-    # use whatever logic you need to determine whether or not this module
-    # made any modifications to your target
-    if module.params['new']:
-        result['changed'] = True
-
-    # during the execution of the module, if there is an exception or a
-    # conditional state that effectively causes a failure, run
-    # AnsibleModule.fail_json() to pass in the message and the result
-    if module.params['name'] == 'fail me':
-        module.fail_json(msg='You requested this to fail', **result)
+    with open(module.params['trigger_path'],"w") as sysrq_trigger:
+        sysrq_trigger.write("t\n")
+    result['message'] = 'Kernel task dump triggered'
+    result['changed'] = True
 
     # in the event of a successful module execution, you will want to
     # simple AnsibleModule.exit_json(), passing the key/value results
